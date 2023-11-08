5b5958f6734aeb3d4474f434859a8e68302670c4
calvin d. woo <calvin.woo@ww.com>
2023-11-08 18:47:54 -0500

    accidental delete

---
+++ tests/test_lexer.py
@@ -0,0 +1,50 @@
+import os
+import pytest
+
+from etalang.lexer import lexer
+from etalang.utils import find_column
+
+
+@pytest.mark.parametrize("source", [
+    "add",
+    "arrayinit",
+    "arrayinit2",
+    "ex1",
+    "ex2",
+    # "gcd",
+])
+def test_lexer(source):
+    source_f = os.path.join(os.path.dirname(__file__), f"source/{source}.eta")
+    with open(source_f, "r") as f:
+        source_s = f.read()
+
+    lexer.lineno = 1   # resets lexer position
+    lexer.input(source_s)
+
+    output_s = ""
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+
+        if tok.type == "ID":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} id {tok.value}\n"
+        elif tok.type == "INTEGER":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} integer {tok.value}\n"
+        elif tok.type == "STRING":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} string {tok.value}\n"
+        elif tok.type == "CHARACTER":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} character {tok.value}\n"
+        elif tok.type == "ERROR":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} error:{tok.value}\n"
+            break
+        else:
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} {tok.value}\n"
+
+    print(output_s)
+
+    lexed_f = os.path.join(os.path.dirname(__file__), f"lexed/{source}.lexed")
+    with open(lexed_f, "r") as f:
+        lexed_s = f.read()
+
+    assert output_s == lexed_s
