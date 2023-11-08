5d4dd358bf1d20c2640ffc1fdb4dc5a9f40307ae
calvin d. woo <calvin.woo@ww.com>
2023-11-08 18:40:41 -0500

    working through tests

--- etalang/lexer.py
+++ etalang/lexer.py
@@ -1,20 +1,36 @@
+import re
 import ply.lex as lex
 
 
 # reserved words
 reserved = {
+    "use": "USE",
+    "if": "IF",
+    "while": "WHILE",
+    "else": "ELSE",
+    "return": "RETURN",
+    "length": "LENGTH",
     "int": "INTTYPE",
     "bool": "BOOLTYPE",
+    "true": "TRUE",
+    "false": "FALSE",
 }
 
 # token types
 tokens = [
     "ID",
     "INTEGER",
+    "CHARACTER",
     "STRING",
     "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
     "EQUAL",
+    "LT",
+    "GT",
     "COLON",
+    "SEMICOLON",
     "COMMA",
     "LPAREN",
     "RPAREN",
@@ -22,16 +38,21 @@ tokens = [
     "RSQBRACKET",
     "LBRACE",
     "RBRACE",
+    "ERROR",
 ] + list(reserved.values())
 
 # regular expression rules
 t_INTEGER = r'0|-?[1-9]([0-9])*'
-# t_STRING = r'\"\\.\"'
-t_STRING = r'\"(\\.|[^"\\])*\"'
 
 t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
 t_EQUAL = r'\='
+t_LT = r'\<'
+t_GT = r'\>'
 t_COLON = r'\:'
+t_SEMICOLON = r'\;'
 t_COMMA = r'\,'
 
 t_LPAREN  = r'\('
@@ -40,6 +61,27 @@ t_LSQBRACKET = r'\['
 t_RSQBRACKET = r'\]'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
+
+# comments to be discarded
+def t_COMMENT(t):
+    r'\/\/.*'
+    pass
+
+def t_STRING(t):
+    r'\"(\\.|[^"\\])*\"'
+    t.value = t.value.strip('\"')
+    # # rip off all \x{y} -> \xy
+    ripped_val = re.sub(r'\\x\{([0-9a-fA-F]+)\}', lambda x: r'\x' + x.group(1), t.value).encode().decode("unicode_escape")
+    t.value = repr(ripped_val).strip('\'')
+    return t
+
+def t_CHARACTER(t):
+    r'\'.?\''
+    t.value = t.value.strip('\'')
+    if len(t.value) == 0:
+        t.type = "ERROR"
+        t.value = "empty character literal"
+    return t
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -6,14 +6,19 @@ from etalang.utils import find_column
 
 
 @pytest.mark.parametrize("source", [
-    # "add",
+    "add",
     "arrayinit",
+    "arrayinit2",
+    "ex1",
+    "ex2",
+    # "gcd",
 ])
 def test_lexer(source):
     source_f = os.path.join(os.path.dirname(__file__), f"source/{source}.eta")
     with open(source_f, "r") as f:
         source_s = f.read()
 
+    lexer.lineno = 1   # resets lexer position
     lexer.input(source_s)
 
     output_s = ""
@@ -21,7 +26,20 @@ def test_lexer(source):
         tok = lexer.token()
         if not tok:
             break
-        output_s += f"{tok.lineno}:{find_column(source_s, tok)} {tok.type} {tok.value}\n"
+
+        if tok.type == "ID":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} id {tok.value}\n"
+        elif tok.type == "INTEGER":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} integer {tok.value}\n"
+        elif tok.type == "STRING":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} string {tok.value}\n"
+        elif tok.type == "CHARACTER":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} character {tok.value}\n"
+        elif tok.type == "ERROR":
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} error:{tok.value}\n"
+            break
+        else:
+            output_s += f"{tok.lineno}:{find_column(source_s, tok)} {tok.value}\n"
 
     print(output_s)
 
